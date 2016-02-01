ac748f901de8fef418a05863b1c43528220c4626
Chris Smith <csmith@scottlogic.com>
2016-02-01 16:07:51 +0000

    Test Parse method (operates on a slice of strings)

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -39,9 +39,15 @@ func TestParseLine(t *testing.T) {
     checkLine("TICKER", []int { 2015, 5, 12, 21, 13, 31 }, 21.123, 34, tick, t) 
 }
 
-/*
 var lines = []string {
     "AA,0,20160127,093001,7.070000000,500",
     "AA,0,20160127,093009,7.060000000,100",
     "AA,0,20160127,093100,7.050000000,100",
-}*/
+}
+
+func TestParse(t *testing.T) {
+    ticks := Parse(lines)
+    checkLine("AA", []int { 2016, 1, 27, 9, 30, 1 }, 7.07, 500, ticks[0], t)
+    checkLine("AA", []int { 2016, 1, 27, 9, 30, 9 }, 7.06, 100, ticks[1], t)
+    checkLine("AA", []int { 2016, 1, 27, 9, 31, 0 }, 7.05, 100, ticks[2], t)
+}
